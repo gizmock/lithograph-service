1c3cce1fabfc662c599a3886157039bed0a3574c
kent-hamaguchi <ismism0@gmail.com>
2021-01-31 20:38:44 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/gizmock/lithograph
+
+go 1.15
+
+require (
+	github.com/aws/aws-lambda-go v1.22.0
+	github.com/kr/text v0.2.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
---
+++ render/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"html/template"
+	"net/http"
+	"time"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(handler)
+}
+
+type response struct {
+	Body            string            `json:"body"`
+	Headers         map[string]string `json:"headers"`
+	Cookies         []string          `json:"cookies"`
+	IsBase64Encoded bool              `json:"isBase64Encoded"`
+	StatusCode      int               `json:"statusCode"`
+}
+
+func handler(ctx context.Context) (response, error) {
+	r := response{
+		Headers: map[string]string{
+			"Content-Type": "text/html",
+		},
+		Cookies:         []string{},
+		IsBase64Encoded: false,
+	}
+	b, err := createBody()
+	if err != nil {
+		r.StatusCode = http.StatusInternalServerError
+		return r, err
+	}
+	r.Body = b
+	r.StatusCode = http.StatusOK
+	return r, nil
+}
+
+type data struct {
+	Title   string
+	Message string
+	Time    time.Time
+}
+
+func createBody() (string, error) {
+	t, err := template.New("tmpl").Parse(html)
+	if err != nil {
+		return "err1", err
+	}
+	var w bytes.Buffer
+	d := data{
+		Title:   "サーバーレスCMSによるサイト",
+		Message: "ようこそ",
+		Time:    time.Now(),
+	}
+	if err := t.Execute(&w, d); err != nil {
+		return "err2", err
+	}
+	return w.String(), nil
+}
+
+const html = `<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>{{.Title}}</title>
+</head>
+<body>
+<h1>{{.Title}}</h1>
+<p>{{.Message}}</p>
+<p>米国の現在時間は {{.Time.Format "2006/1/2 15:04:05"}}</p>
+</body>
+</html>
+`
